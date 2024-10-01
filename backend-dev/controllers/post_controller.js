23829a4e1b4ccfef8e07fcec53e3dca3f5bacd3c
Guna-P-21 <gunap2206@gmail.com>
2024-10-01 10:45:34 +0530

    3-like post mutation added successfully

--- backend-dev/controllers/post_controller.js
+++ backend-dev/controllers/post_controller.js
@@ -123,7 +123,9 @@ export const likeUnlikePost = async (req, res) => {
       // unlike post
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
       await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-      res.status(200).json({ message: "Post Unliked successfully" });
+
+      const updatedLikes = post.likes.filter((id)=> id.toString() !== userId.toString());
+      res.status(200).json(updatedLikes)
     } else {
       // Like post
       post.likes.push(userId);
@@ -138,7 +140,8 @@ export const likeUnlikePost = async (req, res) => {
       });
       await notification.save();
       // response back to the client
-      res.status(200).json({ message: "Post liked Successfully" });
+      const updatedLikes = post.likes
+      res.status(200).json(updatedLikes);
     }
   } catch (error) {
     console.log("Error in likeUnlikePost controller: ", error);
