3459e39d1420ee10c0da653059f63a627bb0a876
Guna-P-21 <gunap2206@gmail.com>
2024-10-08 18:49:47 +0530

    3-changes in App.jsx

--- frontend-dev/src/App.jsx
+++ frontend-dev/src/App.jsx
@@ -4,10 +4,11 @@ import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import NotificationPage from "./pages/notification/NotificationPage";
-import ProfilePage from "./pages/profile/profilePage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
+
 import { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
